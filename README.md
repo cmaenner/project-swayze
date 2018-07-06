7102a239e0bfe04dc836bac7e863c03e079a1e81
Chris Maenner <c.maenner@gmail.com>
2018-07-06 18:37:35 -0400

    Added Facebook authentication and SSL support

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 If you want the ultimate, you've got to be willing to pay the ultimate price. It's not tragic to die doing what you love.
 
 ![alt text](pics/brodhi.jpg "Bodhi")
+
+# Reference (https://coreos.com/os/docs/latest/generate-self-signed-certificates.html)
+    cfssl print-defaults config > ca-config.json
+    cfssl print-defaults csr > ca-csr.json
+    cfssl gencert -initca ca-csr.json | cfssljson -bare ca -
+
+# Generate certificate authority key
+    openssl genrsa -out ca.key 4096
+
+# Generate certificate authority certificate
+    openssl req -new -sha256 -key ca.key -out ca.pem -subj "/C=US/ST=Pennsylvania/L=Philadelphia/O=dafinga.net/OU=ProjectSwayze/CN=localhost"
--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import json
 import logging
 import os
+import ssl
 import sys
-import tornado.escape
+from tornado.auth import FacebookGraphMixin
+from tornado.escape import json_decode, json_encode, url_escape
 from tornado.httpclient import AsyncHTTPClient
+from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
-from tornado.web import Application, RequestHandler, RedirectHandler, url, asynchronous, HTTPError
+from tornado.options import define, options, parse_command_line
+from tornado.web import Application, authenticated, RequestHandler, RedirectHandler, url, asynchronous, HTTPError
+
+# Default CLI parameters
+define("port", default=443, help="run on the given port", type=int)
+define("facebook_api_key", help="your Facebook application API key", type=str)
+define("facebook_secret", help="your Facebook application secret", type=str)
 
 # Logging variables
 logLevel = logging.DEBUG
@@ -18,8 +27,17 @@ try:
 except:
     sys.exit(1)
 
+
+class BaseHandler(RequestHandler):
+    def get_current_user(self):
+        user_json = self.get_secure_cookie("swayze")
+        if not user_json:
+            return None
+        return json_decode(user_json)
+
 class MainHandler(RequestHandler):
     @asynchronous
+    @authenticated
     def get(self):
         https = AsyncHTTPClient()
         https.fetch("https://data.cityofnewyork.us/api/views/kku6-nxdu/rows.json?accessType=DOWNLOAD", callback=self.on_response)
@@ -27,7 +45,7 @@ class MainHandler(RequestHandler):
     def on_response(self, response):
         if response.error:
             raise HTTPError(500)
-        json = tornado.escape.json_decode(response.body)
+        json = json_decode(response.body)
         self.write(json["meta"]["view"])
         self.finish()
 
@@ -38,19 +56,56 @@ class WebApp(Application):
         # How Tornado performs mappings between URLs and handlers
         handlers = [
             (r"/", MainHandler),
-            url(r"/app", RedirectHandler, {"url": "https://www.imdb.com/title/tt0102685/"})
+            url(r"/app", RedirectHandler, {"url": "https://www.imdb.com/title/tt0102685/"}),
+            (r"/auth/login", AuthLoginHandler),
+            (r"/auth/logout", AuthLogoutHandler)
         ]
 
         # Tornado settings
         settings = {
+            "autoescape": None,
             "debug": True,
+            "facebook_api_key": options.facebook_api_key,
+            "facebook_secret": options.facebook_secret,
+            "login_url": "/auth/login",
             "xsrf_cookies": True
         }
         # Used for single inheritance to call parent class
         super().__init__(handlers, **settings)
 
+class AuthLoginHandler(BaseHandler, FacebookGraphMixin):
+    async def get(self):
+        my_url = (self.request.protocol + "://" + self.request.host +
+                  "/auth/login?next=" +
+                  url_escape(self.get_argument("next", "/")))
+        if self.get_argument("code", False):
+            user = await self.get_authenticated_user(
+                redirect_uri=my_url,
+                client_id=self.settings["facebook_api_key"],
+                client_secret=self.settings["facebook_secret"],
+                code=self.get_argument("code"))
+            self.set_secure_cookie(
+                "fbdemo_user", json_encode(user))
+            self.redirect(self.get_argument("next", "/"))
+            return
+        self.authorize_redirect(redirect_uri=my_url,
+                                client_id=self.settings["facebook_api_key"],
+                                extra_params={"scope": "user_posts"})
+
+class AuthLogoutHandler(BaseHandler, FacebookGraphMixin):
+    def get(self):
+        self.clear_cookie("fbdemo_user")
+        self.redirect(self.get_argument("next", "/"))
+
 def main():
-    WebApp().listen(8888)
+    parse_command_line()
+    if not (options.facebook_api_key and options.facebook_secret):
+        print("--facebook_api_key and --facebook_secret must be set")
+        return
+    ssl_ctx = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    ssl_ctx.load_cert_chain(os.path.join("./server", "server.pem"), os.path.join("./server", "server-key.pem"))
+    http_server = HTTPServer(WebApp(), ssl_options=ssl_ctx)
+    http_server.listen(options.port)
     IOLoop.current().start()
 
 if __name__ == "__main__":
