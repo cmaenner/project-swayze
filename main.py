2e9c21db6076a99fac712d8f81a0f212606d869e
Chris Maenner <c.maenner@gmail.com>
2018-07-15 19:22:54 -0400

    Facebook auth is working

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+
+__author__ = "Chris Maenner"
+__copyright__ = "Copyright 2018, The Swayze Project"
+__credits__ = ["Chris Maenner"]
+__license__ = "GPL"
+__version__ = "0.0.1"
+__maintainer__ = "Chris Maenner"
+__email__ = "christopher@dafinga.net"
+__status__ = "Development"
+
+import base64
 import json
 import logging
 import os
 import ssl
 import sys
-from tornado.auth import FacebookGraphMixin
-from tornado.escape import json_decode, json_encode, url_escape
-from tornado.gen import coroutine
-from tornado.httpclient import AsyncHTTPClient
-from tornado.httpserver import HTTPServer
-from tornado.ioloop import IOLoop
-from tornado.options import define, options, parse_command_line
-from tornado.web import Application, authenticated, RequestHandler, RedirectHandler, url, asynchronous, HTTPError, UIModule
+import os.path
+import tornado.auth
+import tornado.escape
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+from tornado.options import define, options
 
 # Default CLI parameters
 define("port", default=443, help="run on the given port", type=int)
@@ -28,92 +41,98 @@ try:
 except:
     sys.exit(1)
 
-class BaseHandler(RequestHandler):
-    def get_current_user(self):
-        user_json = self.get_secure_cookie("fbdemo_user")
-        if not user_json:
-            return None
-        return json_decode(user_json)
-
-class MainHandler(RequestHandler):
-    @asynchronous
-    @authenticated
-    def get(self):
-        https = AsyncHTTPClient()
-        https.fetch("https://data.cityofnewyork.us/api/views/kku6-nxdu/rows.json?accessType=DOWNLOAD", callback=self.on_response)
-
-    def on_response(self, response):
-        if response.error:
-            raise HTTPError(500)
-        json = json_decode(response.body)
-        self.write(json["meta"]["view"])
-        self.finish()
-
-class WebApp(Application):
+class Application(tornado.web.Application):
     def __init__(self):
         """Responsible for global configuration, including the routing table that maps requests to handlers"""
 
         # How Tornado performs mappings between URLs and handlers
         handlers = [
             (r"/", MainHandler),
-            url(r"/app", RedirectHandler, {"url": "https://www.imdb.com/title/tt0102685/"}),
+            tornado.web.url(r"/app", tornado.web.RedirectHandler, {"url": "https://www.imdb.com/title/tt0102685/"}),
             (r"/auth/login", AuthLoginHandler),
-            (r"/auth/logout", AuthLogoutHandler)
+            (r"/auth/logout", AuthLogoutHandler),
         ]
 
         # Tornado settings
         settings = {
-            "autoescape": None,
             "cookie_secret": "__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
-            "debug": True,
+            "login_url": "/auth/login",
+            "template_path": os.path.join(os.path.dirname(__file__), "templates"),
+            "static_path": os.path.join(os.path.dirname(__file__), "static"),
+            "xsrf_cookies": True,
             "facebook_api_key": options.facebook_api_key,
             "facebook_secret": options.facebook_secret,
-            "login_url": "/auth/login",
-            "static_path": os.path.join(os.path.dirname(__file__), "static"),
-            "template_path": os.path.join(os.path.dirname(__file__), "templates"),
-            "xsrf_cookies": True,
-            "ui_modules": {"Post": PostModule}
+            "ui_modules": {"Post": PostModule},
+            "dubug": True,
+            "autoescape": None
         }
 
         # Used for single inheritance to call parent class
-        super().__init__(handlers, **settings)
+        tornado.web.Application.__init__(self, handlers, **settings)
 
-class AuthLoginHandler(BaseHandler, FacebookGraphMixin):
-    @coroutine
-    def get(self):
+class BaseHandler(tornado.web.RequestHandler):
+    def get_current_user(self):
+        # user_json = self.get_secure_cookie("fbdemo_user")
+        user_json = self.get_secure_cookie("user")
+        if not user_json:
+            return None
+        return tornado.escape.json_decode(user_json)
+
+class MainHandler(BaseHandler, tornado.auth.FacebookGraphMixin):
+    @tornado.web.authenticated
+    @tornado.web.asynchronous
+    async def get(self):
+        https = tornado.httpclient.AsyncHTTPClient()
+        await https.fetch("https://data.cityofnewyork.us/api/views/kku6-nxdu/rows.json?accessType=DOWNLOAD", callback=self.on_response)
+
+    def on_response(self, response):
+        if response.error:
+            raise tornado.web.HTTPError(500)
+        json = tornado.escape.json_decode(response.body)
+        self.write(json["meta"]["view"])
+        self.finish()
+
+class AuthLoginHandler(BaseHandler, tornado.auth.FacebookGraphMixin):
+    async def get(self):
+        my_url = (self.request.protocol + "://" + self.request.host +
+                  "/auth/login?next=" +
+                  tornado.escape.url_escape(self.get_argument("next", "/")))
         if self.get_argument("code", False):
-            user = yield self.get_authenticated_user(
-                redirect_uri='https://localhost/',
+            user = await self.get_authenticated_user(
+                redirect_uri=my_url,
                 client_id=self.settings["facebook_api_key"],
                 client_secret=self.settings["facebook_secret"],
                 code=self.get_argument("code"))
-        # Save the user with e.g. set_secure_cookie
-        else:
-            yield self.authorize_redirect(
-                redirect_uri='https://localhost/',
-                client_id=self.settings["facebook_api_key"])
-                # extra_params={"scope": "read_stream,offline_access"})
-                # code=self.get_argument("code"))
+            # self.set_secure_cookie("fbdemo_user", tornado.escape.json_encode(user))
+            self.set_secure_cookie("user", tornado.escape.json_encode(user))
+            self.redirect(self.get_argument("next", "/"))
+            return
+        self.authorize_redirect(
+            redirect_uri=my_url,
+            client_id=self.settings["facebook_api_key"],
+            extra_params={"scope": "user_posts"}
+        )
 
-class AuthLogoutHandler(BaseHandler, FacebookGraphMixin):
+class AuthLogoutHandler(BaseHandler, tornado.auth.FacebookGraphMixin):
     def get(self):
-        self.clear_cookie("fbdemo_user")
+        # self.clear_cookie("fbdemo_user")
+        self.clear_cookie("user")
         self.redirect(self.get_argument("next", "/"))
 
-class PostModule(UIModule):
+class PostModule(tornado.web.UIModule):
     def render(self, post):
         return self.render_string("modules/post.html", post=post)
 
 def main():
-    parse_command_line()
+    tornado.options.parse_command_line()
     if not (options.facebook_api_key and options.facebook_secret):
-        print("--facebook_api_key and --facebook_secret must be set")
+        logging.info("--facebook_api_key and --facebook_secret must be set")
         return
     ssl_ctx = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
     ssl_ctx.load_cert_chain(os.path.join("./server", "server.pem"), os.path.join("./server", "server-key.pem"))
-    http_server = HTTPServer(WebApp(), ssl_options=ssl_ctx)
+    http_server = tornado.httpserver.HTTPServer(Application(), ssl_options=ssl_ctx)
     http_server.listen(options.port)
-    IOLoop.current().start()
+    tornado.ioloop.IOLoop.current().start()
 
 if __name__ == "__main__":
     main()
