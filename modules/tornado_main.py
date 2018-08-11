347644e7f002c42ce6b67cfec7ee9c532bc35966
Chris Maenner <c.maenner@gmail.com>
2018-08-11 18:12:19 -0400

    Added module support

--- modules/tornado_main.py
+++ modules/tornado_main.py
@@ -1,3 +1,15 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+
+# Modules
+from tornado.escape import json_decode
+from tornado.httpclient import AsyncHTTPClient
+from tornado.ioloop import IOLoop
+from tornado.web import asynchronous, authenticated, HTTPError
+
+# Application modules
+from modules.tornado_authentication import BaseHandler
+
 class MainHandler(BaseHandler):
     """Class used to handle root HTTP requests"""
 
